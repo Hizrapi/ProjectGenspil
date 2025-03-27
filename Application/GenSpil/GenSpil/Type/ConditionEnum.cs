e1db3a79db601bb7c56313d8484920ec36ee00c5
Cecilie <ceciliek1989@outlook.com>
2025-03-27 20:13:01 +0100

    Conditions

---
+++ Application/GenSpil/GenSpil/Type/ConditionEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenSpil.Type
+{
+  public enum ConditionEnum
+    {
+        Ny,
+        Brugt,
+        God,
+        Slidt
+
+    }
+}
