eeb2604dded186c059fbe552524eaa7070e1bff8
Sabine Jensen <ssje71@edu.ucl.dk>
2025-03-28 11:37:56 +0100

    Add Reserve.cs class with members and constructor

---
+++ Application/GenSpil/GenSpil/Model/Reserve.cs
@@ -0,0 +1,30 @@
+﻿
+namespace GenSpil.Model;
+
+public class Reserve
+{
+
+    /// <summary>
+    /// Egenskaber for Reserve class 
+    /// </summary>
+    DateTime _reservedDate { get; set; }
+    int _quantity { get; set; }
+    int _customerID { get; set; }
+    int _boardGameID { get; set; }
+
+    /// <summary>
+    /// Constructor for Reserve class
+    /// </summary>
+    /// <param name="reservedDate"></param>
+    /// <param name="quantity"></param>
+    /// <param name="customerID"></param>
+    /// <param name="boardGameID"></param>
+    public Reserve(DateTime reservedDate, int quantity, int customerID, int boardGameID)
+    {
+        _reservedDate = reservedDate;
+        _quantity = quantity;
+        _customerID = customerID;
+        _boardGameID = boardGameID;
+    }
+
+}
