3dae654d10b1fa1231ad03a69d5ffb54d648c731
Cecilie <ceciliek1989@outlook.com>
2025-03-30 19:57:20 +0200

    Tilføjet summary

--- Application/GenSpil/GenSpil/Program.cs
+++ Application/GenSpil/GenSpil/Program.cs
@@ -5,5 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+       
         }
+
     }
+}
--- Application/GenSpil/GenSpil/Type/ConditionEnum.cs
+++ Application/GenSpil/GenSpil/Type/ConditionEnum.cs
@@ -7,7 +7,11 @@ using System.Threading.Tasks;
 namespace GenSpil.Type
 {
   public enum ConditionEnum
-    {
+
+    /// <summary>
+    /// BoardGames ConditionEnum 
+    /// </summary>
+    { 
         Ny,
         Brugt,
         God,
