c2e6d52ed9003f0a4222e95f1c8f05ea6804125f
msoj71393 <msoj71393@edu.ucl.dk>
2025-03-31 11:38:17 +0200

    #49

---
+++ Application/GenSpil/GenSpil/Model/Customer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace GenSpil.Model
+{
+    internal class Customer
+    {
+        int CustomerID {  get; set; }
+        string Name { get; set; }
+        string Address { get; set; }
+
+        public Customer(int customerID, string name, string address)
+        {
+            CustomerID = customerID;
+            Name = name;
+            Address = address;
+        }
+
+        public static int GetCustomerID(Customer customer)
+        {
+            return customer.CustomerID;
+        }
+
+        public static string GetName(Customer customer)
+        {
+            return customer.Name;
+        }
+
+        public static string GetAddress(Customer customer)
+        {
+            return customer.Address;
+        }
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        public void SetAddress(string address)
+        {
+            Address = address;
+        }
+
+        public override string ToString()
+        {
+            return $"CustomerID: {CustomerID}, Name: {Name}, Address: {Address}";
+        }
+    }
+}
