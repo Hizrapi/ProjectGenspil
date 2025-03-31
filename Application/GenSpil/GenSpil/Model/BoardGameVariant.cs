9c2b387e0d50b000d915437c14a0dbae3950d91b
Martin Lindecrona <martinhersomhansen@gmail.com>
2025-03-31 14:35:05 +0200

    #47, 48,# 45. #69

---
+++ Application/GenSpil/GenSpil/Model/BoardGameVariant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenSpil.Model
+{
+    class BoardGameVariant
+    {
+        public string Title { get; private set; }
+        public string Variant { get; private set; }
+
+        public BoardGameVariant(string title, string variant)
+        {
+            Title = title;
+            Variant = variant;
+        }
+    }
+}
