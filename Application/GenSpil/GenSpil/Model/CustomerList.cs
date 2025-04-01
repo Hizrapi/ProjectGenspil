490773dcba0d9563716e0fa23537607d3ff01b94
msoj71393 <msoj71393@edu.ucl.dk>
2025-04-01 09:44:05 +0200

    #63

---
+++ Application/GenSpil/GenSpil/Model/CustomerList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace GenSpil.Model
+{
+    public class CustomerList
+    {
+        private List<Customer> _customers;
+
+        public CustomerList()
+        {
+            _customers = new List<Customer>();
+        }
+
+        public void AddCustomer(Customer customer)
+        {
+            _customers.Add(customer);
+        }
+
+        public void RemoveCustomer(Customer customer)
+        {
+            _customers.Remove(customer);
+        }
+
+        public Customer? GetCustomerByID(int customerID)
+        {
+            return _customers.Find(c => c.CustomerID == customerID);
+        }
+
+        public void UpdateCustomer(Customer customer) 
+        {
+            
+        }
+
+        public int GenerateID(int customerID)
+        {
+            if (_customers.Count == 0)
+            {
+                return 1;
+            }
+
+            return _customers.Max(c => c.CustomerID) + 1;
+        }
+    }
+}
