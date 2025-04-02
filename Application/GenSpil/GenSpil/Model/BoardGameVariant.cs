689864806b096e9b15d844391369111f1ba84945
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-04-02 10:01:08 +0800

    Change to public class

--- Application/GenSpil/GenSpil/Model/BoardGameVariant.cs
+++ Application/GenSpil/GenSpil/Model/BoardGameVariant.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GenSpil.Model
+﻿namespace GenSpil.Model
 {
-    class BoardGameVariant
+    public class BoardGameVariant
     {
         public string Title { get; private set; }
         public string Variant { get; private set; }
