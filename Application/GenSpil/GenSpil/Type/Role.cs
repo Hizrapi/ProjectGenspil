4d1697177d32da1cc4bfc43dad8799ad3ffc9d74
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-04-02 14:59:40 +0800

    Cleanup

--- Application/GenSpil/GenSpil/Type/Role.cs
+++ Application/GenSpil/GenSpil/Type/Role.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GenSpil.Type;
+﻿namespace GenSpil.Type;
 
 /// <summary>
 /// Role of the user
