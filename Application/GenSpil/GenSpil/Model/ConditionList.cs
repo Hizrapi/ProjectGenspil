1ae8c7e290881c1ccfa31ca0305ece27c7b06447
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-04-02 14:59:09 +0800

    New class

---
+++ Application/GenSpil/GenSpil/Model/ConditionList.cs
@@ -0,0 +1,15 @@
+﻿namespace GenSpil.Model
+{
+    public class ConditionList
+    {
+        List<Condition> _conditions = new List<Condition>();
+
+        public ConditionList()
+        {
+            foreach (Type.Condition condition in Enum.GetValues(typeof(Type.Condition)))
+            {
+                _conditions.Add(new Condition(condition, 0, 0));
+            }
+        }
+    }
+}
