3acc9b05a45eeb251f7c4a9e1bbec07faeb19b6c
msoj71393 <msoj71393@edu.ucl.dk>
2025-04-03 13:31:42 +0200

    #63 Made changes to UpdateCustomer() : void

--- Application/GenSpil/GenSpil/Model/CustomerList.cs
+++ Application/GenSpil/GenSpil/Model/CustomerList.cs
@@ -10,6 +10,7 @@ namespace GenSpil.Model
 {
     public class CustomerList
     {
+        
         private List<Customer> _customers;
 
         public CustomerList()
@@ -17,25 +18,44 @@ namespace GenSpil.Model
             _customers = new List<Customer>();
         }
 
+        //Adds a customer to the list.
         public void AddCustomer(Customer customer)
         {
             _customers.Add(customer);
         }
 
+        //Removes a customer to the list.
         public void RemoveCustomer(Customer customer)
         {
             _customers.Remove(customer);
         }
 
+        //Findes a customer on the list.
         public Customer? GetCustomerByID(int customerID)
         {
             return _customers.Find(c => c.CustomerID == customerID);
         }
 
-        public void UpdateCustomer(Customer customer) 
+        //Update function isn't completed.
+        public void UpdateCustomer(int customerID, string newName, string newAddress) 
         {
-            
+            var customer = _customers.Find(c => c.CustomerID == customerID);
+
+            if (customer != null)
+            {
+                // If customer exists, update the name and address using the setter methods
+                customer.SetName(newName);
+                customer.SetAddress(newAddress);
+
+                Console.WriteLine($"Customer with ID {customerID} has been updated.");
+            }
+            else
+            {
+                // If the customer doesn't exist
+                Console.WriteLine($"Customer with ID {customerID} not found.");
+            }
         }
+
 
         public int GenerateID(int customerID)
         {
