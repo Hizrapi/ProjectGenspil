de21b4f71f45a1dc59780f81be4bcfac9e35077b
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-04-05 10:11:42 +0800

    Change to get and set

--- Application/GenSpil/GenSpil/Model/BoardGameVariant.cs
+++ Application/GenSpil/GenSpil/Model/BoardGameVariant.cs
@@ -3,12 +3,23 @@
     public class BoardGameVariant
     {
         public string Title { get; private set; }
+        public Reserve Reserved { get; private set; } = null!; // Initialize to null to avoid unassigned variable error
         public ConditionList ConditionList { get; private set; }
 
         public BoardGameVariant(string title, ConditionList conditionList)
         {
             Title = title;
             ConditionList = conditionList;
+        }
+
+        public Reserve GetReserved()
+        {
+            return Reserved;
+        }
+
+        public void SetReserved(Reserve reserved)
+        {
+            Reserved = reserved;
         }
     }
 }
